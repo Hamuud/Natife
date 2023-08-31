09adad53534c96e5b076783427f41476247de274
Hamuud <artemlask2014@gmail.com>
2023-08-31 12:50:25 +0300

    Initial Commit

---
+++ Natify_Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Natify_Test
+//
+//  Created by Артем Лясковець on 31.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Natify_Test/Natify_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Natify_TestApp.swift
+//  Natify_Test
+//
+//  Created by Артем Лясковець on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Natify_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
